697d6b9e236d14dd4984187f9e635e253295941f
Paul <paul.cwic@gmail.com>
2024-12-20 19:10:49 +0800

    Added server port

--- app.py
+++ app.py
@@ -234,5 +234,5 @@ def generate_image_route():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True, use_reloader=False)
 
